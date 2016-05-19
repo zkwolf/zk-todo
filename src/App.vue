836e00b6ecf5b77e0df6ceb2b0fe097818afa036
zkwolf <chenhao5866@gmail.com>
2016-05-19 21:27:18 +0800

    change component structure

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,17 @@
 <template>
   <sidebar :todo-types="todoTypes"></sidebar>
   <div id="app">
+    <div class="addTodo" @click="addTodoShow = true">+</div>
     <todo :todos="todos" :todo-types="todoTypes"></todo>
+    <div id="modal" v-show="addTodoShow">
+      <div id="modal-container">
+        <div v-for="todoType in todoTypes">
+          <input type="radio" v-model="picked" value="{{ todoType }}" id="{{ todoType }}">
+          <label for="{{ todoType }}">{{ todoType }}</label>
+        </div>
+        <input type="text" v-model="newTodo" @keyup.enter="addTodo" @keyup.esc="addTodoShow = false" id="addTodo">
+      </div>
+    </div>
   </div>
 </template>
 
@@ -17,7 +27,22 @@ export default {
   data () {
     return {
       todoTypes: ['Work', 'Life', 'Study'],
-      todos: []
+      todos: [],
+      addTodoShow: ''
+    }
+  },
+  methods: {
+    addTodo: function () {
+      let text = this.newTodo.trim()
+      let picked = this.picked
+      if (text) {
+        if (picked) {
+          let todo_item = {}
+          todo_item[picked] = text
+          this.todos.push(todo_item)
+          this.newTodo = ''
+        }
+      }
     }
   }
 }
@@ -49,8 +74,34 @@ body {
   text-decoration: none;
 }
 
-.logo {
-  width: 100px;
-  height: 100px
+.addTodo {
+  font-size: 2rem;
+  cursor: pointer;
+}
+
+#modal {
+  position: fixed;
+  z-index: 9999;
+  left: 18rem;
+  top: 0;
+  height: 100%;
+  width: calc(100% - 18rem);
+  background-color: rgba(0, 0, 0, 0.5);
+  transition: opacity .3s ease;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+}
+
+#modal-container {
+  margin: 0 auto;
+  padding: 10px;
+  width: 500px;
+  height: 300px;
+  background-color: #fff;
+}
+
+#addTodo {
+  width: 400px;
 }
 </style>
--- src/components/Todo.vue
+++ src/components/Todo.vue
@@ -1,18 +1,8 @@
-<template>
-  <div class="addTodo" @click="addTodoShow = true">+</div>
+<template>  
   <div class="todo" v-for="todo in todos">
     <div class="todo_item" v-for="value in todo">
       {{ value }}
       <span @click="removeTodo($index)">X</span>
-    </div>
-  </div>
-  <div id="modal" v-show="addTodoShow">
-    <div id="modal-container">
-      <div v-for="todoType in todoTypes">
-        <input type="radio" v-model="picked" value="{{ todoType }}" id="{{ todoType }}">
-        <label for="{{ todoType }}">{{ todoType }}</label>
-      </div>
-      <input type="text" v-model="newTodo" @keyup.enter="addTodo" @keyup.esc="addTodoShow = false" id="addTodo">
     </div>
   </div>
 </template>
@@ -22,7 +12,6 @@ export default {
   data () {
     return {
       newTodo: '',
-      addTodoShow: false,
       picked: ''
     }
   },
@@ -31,18 +20,6 @@ export default {
     todoTypes: '',
   },
   methods: {
-    addTodo: function () {
-      let text = this.newTodo.trim()
-      let picked = this.picked
-      if (text) {
-        if (picked) {
-          let todo_item = {}
-          todo_item[picked] = text
-          this.todos.push(todo_item)
-          this.newTodo = ''
-        }
-      }
-    },
     removeTodo: function (index) {
       this.todos.splice(index, 1);
     }
@@ -52,45 +29,19 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-  #addTodo {
-    width: 400px;
-    
-  }
+.todo {
+  font-size: 1.4rem;
+  background-color: rgba(0, 0, 0, 0.6);
+  line-height: 2;
+  width: 100%;
+  color: #fff;
+}
 
-  .todo {
-    font-size: 1.4rem;
-    background-color: rgba(0, 0, 0, 0.6);
-    line-height: 2;
-    width: 100%;
-    color: #fff;
-  }
+.todo span {
+  cursor: pointer;
+}
 
-  .todo span {
-    cursor: pointer;
-  }
-  .addTodo {
-    font-size: 2rem;
-  }
-  
-  #modal {
-    position: fixed;
-    z-index: 9999;
-    left: 18rem;
-    top: 0;
-    height: 100%;
-    width: calc(100% - 18rem);
-    background-color: rgba(0, 0, 0, 0.5);
-    transition: opacity .3s ease;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-  }
-  
-  #modal-container {
-    margin: 0 auto;
-    padding: 10px;
-    width: 500px;
-    height: 300px;
-    background-color: #fff;
-  }
+.todo_item {
+  padding-left: 10px;
+}
 </style>
